b84fa3ab89abc740254e5599f0a9d10e204547b9
simonryrie <simonryrie@gmail.com>
2023-06-13 11:19:46 +0100

    Initial HTML and CSS for player card

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import PlayerCard from './components/PlayerCard'
 
 function App() {
 
   return (
     <>
-
+      <PlayerCard></PlayerCard>
     </>
   )
 }
--- myapp/src/components/PlayerCard.jsx
+++ myapp/src/components/PlayerCard.jsx
@@ -1,6 +1,33 @@
 function PlayerCard() {
   return (
-    <div>PlayerCard</div>
+    <div className="card-front column">
+      <h2 className="card__title card-text">Gronk</h2>
+      <img className="card__img"></img>
+      <div className="row">
+        <p className="card__skill-name card-text">Shapeshifting
+          <span className="card__skill-value card-text">92</span>
+        </p>
+        <button type="button" className="card__skill-button"></button>
+      </div>
+      <div className="row">
+        <p className="card__skill-name card-text">Shapeshifting
+          <span className="card__skill-value card-text">92</span>
+        </p>
+        <button type="button" className="card__skill-button"></button>
+      </div>
+      <div className="row">
+        <p className="card__skill-name card-text">Shapeshifting
+          <span className="card__skill-value card-text">92</span>
+        </p>
+        <button type="button" className="card__skill-button"></button>
+      </div>
+      <div className="row">
+        <p className="card__skill-name card-text">Shapeshifting
+          <span className="card__skill-value card-text">92</span>
+        </p>
+        <button type="button" className="card__skill-button"></button>
+      </div>
+    </div>
   )
 }
 
