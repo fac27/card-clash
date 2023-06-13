8e6110c7afccc205fc7baabf30a94c1f1fc7a1db
eliazzo <bethazz@hotmail.co.uk>
2023-06-13 11:09:58 +0100

    data.js aliens array

---
+++ myapp/src/components/data.js
@@ -0,0 +1,72 @@
+export default [
+  {
+    name: "Zogar",
+    slimeFactor: 80,
+    laserPower: 90,
+    telepathy: 70,
+    agility: 85
+  },
+  {
+    name: "Xelnar",
+    slimeFactor: 60,
+    laserPower: 75,
+    telepathy: 85,
+    agility: 70
+  },
+  {
+    name: "Gloptar",
+    slimeFactor: 90,
+    laserPower: 80,
+    telepathy: 60,
+    agility: 75
+  },
+  {
+    name: "Vorblax",
+    slimeFactor: 70,
+    laserPower: 65,
+    telepathy: 80,
+    agility: 90
+  },
+  {
+    name: "Quixor",
+    slimeFactor: 50,
+    laserPower: 70,
+    telepathy: 75,
+    agility: 80
+  },
+  {
+    name: "Zynthor",
+    slimeFactor: 85,
+    laserPower: 75,
+    telepathy: 90,
+    agility: 60
+  },
+  {
+    name: "Nexara",
+    slimeFactor: 75,
+    laserPower: 85,
+    telepathy: 70,
+    agility: 65
+  },
+  {
+    name: "Blorgon",
+    slimeFactor: 95,
+    laserPower: 60,
+    telepathy: 75,
+    agility: 80
+  },
+  {
+    name: "Krydon",
+    slimeFactor: 65,
+    laserPower: 80,
+    telepathy: 90,
+    agility: 70
+  },
+  {
+    name: "Jazmog",
+    slimeFactor: 80,
+    laserPower: 70,
+    telepathy: 75,
+    agility: 85
+  }
+];
