525db494e545dd9a079973ecf11909a9214b535f
simonryrie <simonryrie@gmail.com>
2023-06-13 12:29:23 +0100

    Separated CardFront and CardBack components with player props

--- myapp/src/components/PlayerCard.jsx
+++ myapp/src/components/PlayerCard.jsx
@@ -1,34 +1,11 @@
-function PlayerCard() {
+import CardBack from "./CardBack"
+import CardFront from "./CardFront"
+
+
+export default function PlayerCard() {
   return (
-    <div className="player-card card-front column">
-      <h2 className="card__title card-text no-top-margin">Gronk</h2>
-      <img className="card__img"></img>
-      <div className="card__skill row space-between stack-sm">
-        <label className="card__skill-name card-text">Shapeshifting
-          <span className="card__skill-value card-text">92</span>
-        </label>
-        <input type="radio" className="card__skill-button"></input>
-      </div>
-      <div className="card__skill row space-between">
-        <label className="card__skill-name card-text">Shapeshifting
-          <span className="card__skill-value card-text">92</span>
-        </label>
-        <input type="radio" className="card__skill-button"></input>
-      </div>
-      <div className="card__skill row space-between">
-        <label className="card__skill-name card-text">Shapeshifting
-          <span className="card__skill-value card-text">92</span>
-        </label>
-        <input type="radio" className="card__skill-button"></input>
-      </div>
-      <div className="card__skill row space-between">
-        <label className="card__skill-name card-text">Shapeshifting
-          <span className="card__skill-value card-text">92</span>
-        </label>
-        <input type="radio" className="card__skill-button"></input>
-      </div>
-    </div>
+    <>
+      <CardFront player="player"></CardFront>
+    </>
   )
 }
-
-export default PlayerCard
