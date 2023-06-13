2cc4953136d523ffa73abee0a3bbf408403d56b8
eliazzo <bethazz@hotmail.co.uk>
2023-06-13 13:02:21 +0100

    remove Shuffle() from App.jsx

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -3,11 +3,9 @@ import Scoreboard from './components/Scoreboard'
 import SubmitButton from './components/SubmitButton'
 import ComputerCard from './components/ComputerCard'
 import PlayerCard from './components/PlayerCard'
-import Shuffle from './components/Shuffle'
 
 
 function App() {
-  Shuffle();
 
   return (
     <>
--- myapp/src/components/Shuffle.jsx
+++ myapp/src/components/Shuffle.jsx
@@ -1,17 +1,17 @@
 import aliens from "../data.js";
 
 function Shuffle() {
-  const player1 = [];
+  const playerDeck = [];
 
-  for (let i = 0; i < 5; i++) {
-    const randomIndex = Math.floor(Math.random() * aliens.length) + 1;
-    const alien = aliens.splice(randomIndex, 1)[0];
-    player1.push(alien);
-  }
+    for(let i=0; i<5; i++){
+      const randomIndex = Math.floor(Math.random() * aliens.length);
+      const alien = aliens.splice(randomIndex, 1) [0];
+      playerDeck.push(alien);
+    }
 
-  const computer = aliens.slice();
-
-  return <div></div>;
+  const computerDeck = aliens.slice();
+ 
 }
+
 
 export default Shuffle;
